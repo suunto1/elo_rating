7d57d0827c0091ad6c8e7dabe82c11b1ab79f042
suunto1 <suunto.crb@gmail.com>
2025-01-17 20:46:25 +0200

    rt

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ app.get('/pilots', async (req, res) => {
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).send('Error fetching data');
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+    // } finally {
+    //     if (connection) {
+    //         connection.release();
+    //     }
     }
 });
 
