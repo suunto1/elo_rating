be285f5c5cd5802dfbd0a67e7f2f7e8c14deae7a
suunto1 <suunto.crb@gmail.com>
2025-01-18 01:32:28 +0200

    Revert "new upd"
    This reverts commit 5adcc0f5275a56f53ae6ab1fd31bc445c284469b.

--- server.js
+++ server.js
@@ -19,14 +19,9 @@ app.set('view engine', 'ejs'); // Используем шаблонизатор 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html');
-// });
-
 app.get('/', (req, res) => {
-    res.redirect('/tracks');
+    res.sendFile(__dirname + '/public/index.html');
 });
-
 
 // Маршрут для страницы пилотов
 app.get('/pilots', async (req, res) => {
