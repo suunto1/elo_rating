dde9999e27837c368a85c344d68f5ffd96d94c32
a.shumarin <suunto.crb@gmail.com>
2025-02-12 08:45:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql2/promise');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Настройки подключения к базе данных MySQL
 const dbConfig = {
