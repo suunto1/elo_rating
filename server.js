79140a0bf2837602dec5a302732c098ef032acb2
a.shumarin <suunto.crb@gmail.com>
2025-07-14 15:02:54 +0300

    update connectionLimit to 100

--- server.js
+++ server.js
@@ -42,7 +42,10 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,        
+    connectionLimit: 100,
+    queueLimit: 0
 };
 
 const pool = mysql.createPool(dbConfig);
