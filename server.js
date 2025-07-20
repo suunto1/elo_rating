838330c83fc85fbc61abd167eec92dda0daa9bf6
a.shumarin <suunto.crb@gmail.com>
2025-07-20 14:17:23 +0300

    delete https://elo-rating-1.onrender.com

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ if (!STEAM_API_KEY || !STEAM_RETURN_URL || !SESSION_SECRET || !STEAM_REALM) {
 const cors = require('cors');
 
 app.use(cors({
-    origin: ['https://elo-rating.vercel.app', 'https://elo-rating-1.onrender.com'],
+    origin: ['https://elo-rating.vercel.app'],
     credentials: true
 }));
 
